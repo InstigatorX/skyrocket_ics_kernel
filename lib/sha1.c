86ce6c7e90c99daa268701cc2161054d44f0b98d
InstigatorX <sloebric@gmail.com>
2012-07-01 11:49:04 -0700

    lib/sha1.c: quiet sparse noise about symbol not declared
    Include <linux/cryptohash.h> to pickup the declarations for
    sha_transform
    and sha_init to quite the sparse noise:
    
      warning: symbol 'sha_transform' was not declared. Should it be static?
      warning: symbol 'sha_init' was not declared. Should it be static?
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Acked-by: Mandeep Singh Baines <msb@chromium.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/sha1.c
+++ lib/sha1.c
@@ -8,6 +8,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/bitops.h>
+#include <linux/cryptohash.h>
 #include <asm/unaligned.h>
 
 #define f1(x,y,z)   (z ^ (x & (y ^ z)))		/* x ? y : z */
