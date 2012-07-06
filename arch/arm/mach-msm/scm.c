ddd63e1fc6249fad74c0e243cb8e7e7156140d60
InstigatorX <sloebric@gmail.com>
2012-07-05 23:02:53 -0700

    Remove Toolchain 4.6.3 fixes

--- arch/arm/mach-msm/scm.c
+++ arch/arm/mach-msm/scm.c
@@ -173,7 +173,8 @@ static u32 smc(u32 cmd_addr)
 			__asmeq("%1", "r0")
 			__asmeq("%2", "r1")
 			__asmeq("%3", "r2")
-			".arch_extension sec\n"
+/*			".arch_extension sec\n"
+*/
 			"smc	#0	@ switch to secure world\n"
 			: "=r" (r0)
 			: "r" (r0), "r" (r1), "r" (r2)
@@ -295,7 +296,8 @@ s32 scm_call_atomic1(u32 svc, u32 cmd, u32 arg1)
 		__asmeq("%1", "r0")
 		__asmeq("%2", "r1")
 		__asmeq("%3", "r2")
-		".arch_extension sec\n"
+/*		".arch_extension sec\n"
+*/
 		"smc	#0	@ switch to secure world\n"
 		: "=r" (r0)
 		: "r" (r0), "r" (r1), "r" (r2)
@@ -328,7 +330,8 @@ s32 scm_call_atomic2(u32 svc, u32 cmd, u32 arg1, u32 arg2)
 		__asmeq("%2", "r1")
 		__asmeq("%3", "r2")
 		__asmeq("%4", "r3")
-		".arch_extension sec\n"
+/*		".arch_extension sec\n"
+*/
 		"smc	#0	@ switch to secure world\n"
 		: "=r" (r0)
 		: "r" (r0), "r" (r1), "r" (r2), "r" (r3));
@@ -356,7 +359,8 @@ u32 scm_get_version(void)
 			__asmeq("%1", "r1")
 			__asmeq("%2", "r0")
 			__asmeq("%3", "r1")
-			".arch_extension sec\n"
+/*			".arch_extension sec\n"
+*/
 			"smc	#0	@ switch to secure world\n"
 			: "=r" (r0), "=r" (r1)
 			: "r" (r0), "r" (r1)
